57f41d595858d02a9ec842a785f0669ea9dfb92a
Jacek <jacek.paszek@onet.pl>
2017-06-19 22:36:27 +0200

    function modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 var list = document.getElementById('list');
+var i = document.getElementById('list').childElementCount;
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
   var element = document.createElement('li');
-  var i = document.getElementsByTagName('li').length;
-  element.innerHTML = 'item ' + i;
+  var text = document.createTextNode('item ' + i);  
+  element.appendChild(text);
   list.appendChild(element);
 });
